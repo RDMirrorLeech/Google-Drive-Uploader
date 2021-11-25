5656a9cecc8a1c6cf5b9ca9e5be292a201a07d3d
RDMirrorLeech <92361844+RDMirrorLeech@users.noreply.github.com>
2021-11-25 16:35:38 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "2131326362:AAHIn9z5416LXzVgdchvRec0gljG8i9EqxA"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1cib_V-gxR3CDptmZPh11i-q8ZaMd7-uu"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AMLhN6Aq2yQoUk9PVA"
     
     
     
